a466021055a380848bbd7183df88eb2198aaab18
Hana Hsu <hanahsu@ecoworkinc.com>
2015-02-10 15:18:46 +0800

    oauth: github login

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 trellis
 =======
+
+## oauth login
+以 devise 為架構，加上 omniauth 提供 GitHub 及 Google 帳號登入  
+oauth 授權的使用者，資料庫不記錄 email 及 password
+
+### GitHub
+#### Register new applicatin
+[application register](https://github.com/settings/applications/new)
+
+#### config
+`/config/application.yml`
+
+```
+GITHUB_KEY: "your Client ID here"
+GITHUB_SECRET: "your Client Secret here"
+```
---
+++ app/models/user.rb
@@ -0,0 +1,35 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, :omniauth_providers => [:github]
+
+  def self.find_for_github_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    unless user
+      user = User.create!(
+        name:auth.extra.raw_info.name,
+        provider:auth.provider,
+        uid:auth.uid
+      )
+    end
+    user
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.github_data"] && session["devise.github_data"]["extra"]["raw_info"]
+        user.name = data["name"] if user.name.blank?
+      end
+    end
+  end
+
+  def email_required?
+    super && provider.blank?
+  end
+
+  def password_required?
+    super && provider.blank?
+  end
+end
